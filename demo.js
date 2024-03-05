96e184892001c8451ca8cff359f577b8ce32895c
jueun1007 <zhfvkq1007@naver.com>
2024-03-05 20:36:19 +0900

    firsr commit

---
+++ demo.js
@@ -0,0 +1,22 @@
+// 데이터 타입 
+// string "가나다" 
+// number 1 12 343 
+// boolean 참 거짓 
+true;
+false;
+// null 정의했지만 업는것
+null;  
+// undefined 그냥 아예 정의가 안되어 있는 것 
+undefined;
+
+
+
+// 변수 데이터를 저장해놓는 공간
+height = 180;
+console.log("height"height);
+christ = true;
+
+
+// 함수
+// 조건문
+// 반복문
